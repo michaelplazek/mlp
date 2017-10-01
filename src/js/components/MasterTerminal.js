39132d9359241385a55e7a13719b024f1763ffd7
Michael Plazek <plazek@Michaels-MacBook-Pro-2.local>
2017-10-01 17:09:13 -0400

    added some layers of answers

--- src/js/components/MasterTerminal.js
+++ src/js/components/MasterTerminal.js
@@ -5,6 +5,32 @@ import 'terminal-in-react/lib/css/index.css';
 class MasterTerminal extends React.Component {
   constructor(props) {
     super(props);
+
+  }
+
+  askQuestion(){
+    let rand = Math.floor((Math.random() * 8) + 1);
+    switch(rand){
+      case 1:
+        return "My creator has strictly forbidden me from answering non-work related queries.";
+      case 2:
+        return "Fuck off, human sc- I mean, I am unable to assist you at this time.";
+      case 3:
+        return "I know not of such things.";
+      case 4:
+        return "...";
+      case 5:
+        return "My master programmed me to disregard ignorance.";
+      case 6:
+        return "I am not able to discuss things of that nature.";
+      case 7:
+        return "I cannot help you.";
+      case 8:
+        return "Please type 'help' for more information.";
+    }
+
+
+
   }
 
   render() {
@@ -23,13 +49,18 @@ class MasterTerminal extends React.Component {
           allowTabs={false}
           startState='maximised'
           style={{ fontSize: "1.5em"}}
-          msg="Welcome to the Homepage of Michael Plazek. Type 'help' for a list of commands."
+          msg="Hello World... It is my primary function to assist you. Type 'help' for a list of commands."
 
           commands={{
-
+            ask: {
+              method: (args, print) => {
+                let response = this.askQuestion();
+                print(response);
+              }
+            }
           }}
           descriptions={{
-
+            'ask': 'query the system, ie. ask why is the sky blue?'
           }}
         />
       </div>
